f5fb0c71f4992db9982580785da2109c055d3b62
FrostyFox33 <43838681+FrostyFox33@users.noreply.github.com>
2023-12-21 09:23:50 +0300

    sendo

--- test_model.py
+++ test_model.py
@@ -2,79 +2,54 @@ import numpy as np
 from custom_env import CustomEnv
 
 
-def encode_state(state_dict):
-    # This function needs to match exactly how you encoded states during training
-    return tuple(state_dict["bobbin_positions"])
+q_table = np.load("q_table.npy")  # Load the Q-table
 
 
-def test_model(env, q_table, test_episodes, predefined_states=None):
-    for episode in range(test_episodes):
-        # Initialize state_dict at the beginning of each episode
-        if predefined_states:
-            env.set_predefined_state(
-                predefined_states[episode % len(predefined_states)]
-            )
-            state_dict = env.state
+def test_specific_state(env, q_table, predefined_state):
+    # Set the environment to the predefined state
+    env.set_predefined_state(predefined_state)
+    state_dict = env.set_predefined_state(predefined_state)
+
+    done = False
+    total_reward = 0
+    step_count = 0
+
+    while not done:
+        state = encode_state(state_dict)
+        # Choose the best action based on the Q-table
+        if state in q_table:
+            q_values = q_table[state]
+            action = np.argmax(q_values)
         else:
-            state_dict = env.reset()
+            # Default action if the state is not in the Q-table
+            action = 81
 
-        done = False
-        total_reward = 0
-        step_count = 0
+        # Take the action and observe the outcome
+        new_state_dict, reward, done, _ = env.step(action)
+        total_reward += reward
+        step_count += 1
 
-        print(f"\nTesting Episode {episode + 1}")
+        # Print the step, action, and current state
+        print(f"Step {step_count}: Action taken: {action}")
+        env.render()
+        print(f"Reward for this step: {reward}, Total Reward: {total_reward}")
 
-        while not done:
-            state = encode_state(state_dict)
-            # Ensure that the state is in the Q-table
-            if state in q_table:
-                action = np.argmax(q_table[state])
-            else:
-                # Default action if the state is not in the table
-                action = 81
+        # Update the state
+        state_dict = new_state_dict
 
-            new_state_dict, reward, done, _ = env.step(action)
-            total_reward += reward
-            step_count += 1
-
-            print(f"Step {step_count}: Action taken: {action}")
-            env.render()
-            print(f"Reward for this step: {reward}, Total Reward: {total_reward}")
-
-            state_dict = new_state_dict
-
-        print(
-            f"Test Episode {episode + 1} finished after {step_count} steps. Total Reward: {total_reward}\n"
-        )
+    print(f"Test finished after {step_count} steps. Total Reward: {total_reward}")
 
 
 def main():
-    # Load the trained Q-table (ensure it's the correct file)
     q_table = np.load("q_table.npy", allow_pickle=True)
-
     env = CustomEnv()
-    test_episodes = 10
-    predefined_states = [
-        {
-            "bobbin_positions": [1, 1, 0, 0, 1, 0, 0, 0, 0],
-            "bobbin_priorities": [
-                2,
-                2,
-                0,
-                0,
-                10,
-                0,
-                0,
-                0,
-                0,
-            ],  # Replace with actual priorities
-        },
-        # ... other predefined states ...
-    ]
 
-    # Add more predefined states as needed
+    predefined_state = {
+        "bobbin_positions": [1, 1, 0, 0, 1, 0, 0, 0, 0],
+        "bobbin_priorities": [2, 2, 0, 0, 10, 0, 0, 0, 0],
+    }
 
-    test_model(env, q_table, test_episodes, predefined_states)
+    test_specific_state(env, q_table, predefined_state)
 
     print("Testing completed.\n")
 
